8fc36782ad9090ab696b116d647caff1e4c4812c
Michael Meyer <michael.meyer@improving.com>
2021-06-24 10:53:57 -0500

    initial commit

---
+++ MicrosoftFightSimulator/Models/Character.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MicrosoftFightSimulator.Models
+{
+    public class Character
+    {
+        public CharacterClass CharacterClass { get; set; }
+    }
+}
---
+++ MicrosoftFightSimulator/Models/CharacterClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MicrosoftFightSimulator.Models
+{
+    public class CharacterClass
+    {
+        public short Armor { get; set; }
+        public short HitBonus { get; set; }
+        public short Damage { get; set; }
+        public string Ability { get; set; }
+
+    }
+}
